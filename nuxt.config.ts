d812ffd036894899a93dac9d82aac06cebbdf99e
Nikki Casar <casa0036@algonquinlive.com>
2025-03-28 13:27:36 -0400

    some changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// // https://nuxt.com/docs/api/configuration/nuxt-config
+// export default defineNuxtConfig({
+//   compatibilityDate: '2024-11-01',
+//   devtools: { enabled: true }
+// })
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  app: {
+    baseURL: '/mtm6407-static-site-casa0036/',  // Ensure this matches your repo name
+    buildAssetsDir: "/",
+  },
+  nitro: {
+    preset: 'static',  // Enables static site generation
+  }
 })
